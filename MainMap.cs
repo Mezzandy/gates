5c56d5d8cc997c5853e03d6a5a839a53a64c9ca7
Mezzandy <mezzandy@yahoo.com>
2017-03-23 16:35:00 -0500

    Add files via upload

---
+++ MainMap.cs
@@ -0,0 +1,389 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMap : MonoBehaviour {
+
+	public int TextureWidth;
+	public int TextureHeight;
+
+	public Texture2D MapTexture;
+
+	public int MapSize;
+	public int WorldSeed;
+	public Vector3 NoiseOrigin1;
+	public Vector3 NoiseOrigin2;
+	public Vector3 NoiseOrigin3;
+	public Vector3 NoiseOrigin4;
+	public Vector3 NoiseScale1;
+	public Vector3 NoiseScale2;
+	public Vector3 NoiseScale3;
+	public Vector3 NoiseScale4;
+
+	public float SeaLevel = 0.15f;
+	public float MasterScale;
+	public float WorldTemperature;
+	public float WorldHumidity;
+	public float WorldHeight;
+	public Color[] MapColors;
+
+	public Vector2 vScale;
+	public Vector3 gScale;
+	public float AltF;
+
+	public float BaseT;
+	public float RoughT;
+	public float DetailT;
+	public float NegaT;
+
+//	public float Rain; 
+//	public float RainT;
+//
+//	public float Temp;
+//	public float TempT;
+
+	public Vector2 ViewCenter;
+
+	public float Zoom;
+
+	// Use this for initialization
+	void Start () {
+		MapTexture = new Texture2D (MapSize, MapSize);
+		MapTexture.filterMode = FilterMode.Point;
+		MapTexture.wrapMode = TextureWrapMode.Clamp;
+
+		gameObject.GetComponent<Renderer> ().material.mainTexture = MapTexture;
+
+		RandomColors ();
+
+
+		WorldHumidity = Random.Range (0.75f,1.15f);
+		WorldTemperature = Random.Range (0.75f,1.15f);
+		SeaLevel = Random.Range (0.1f,0.2f);
+		WorldHeight = 1.0f;
+		MasterScale = 1.0f/MapSize;
+
+		SeedValues ();
+		GenerateMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.L)) {
+			RandomColors ();
+		}
+
+		if (Input.GetKeyUp (KeyCode.P)) {
+			SeedValues ();
+			GenerateMap ();
+			AddCities (10);
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			ViewCenter.y += Zoom;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			ViewCenter.y -= Zoom;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			ViewCenter.x -= Zoom;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			ViewCenter.x += Zoom;
+		}
+
+		if (Input.GetKey (KeyCode.LeftBracket)) {
+			Zoom--;
+			if (Zoom < 1) {
+				Zoom = 1.0f;
+			}
+		}
+
+		if (Input.GetKey (KeyCode.RightBracket)) {
+			Zoom++;
+		}
+			
+		UpdateValues ();
+		GenerateMap ();
+
+	}
+		
+
+	public void RandomColors(){
+		for (int x = 0; x < MapColors.Length; x++) {
+			MapColors[x] = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+		}
+	}
+
+	public void SeedValues(){
+		//Set Random Values
+		WorldSeed = Random.Range(int.MinValue, int.MaxValue);
+		Random.seed = WorldSeed;
+
+		WorldHumidity = Random.Range(0.85f,1.15f);
+		WorldTemperature = Random.Range(0.85f,1.15f);
+		WorldHeight = Random.Range(1.0f,1.25f);
+		SeaLevel = Random.Range(0.15f,0.30f);
+
+		NoiseOrigin1.x = Random.Range (0.0f,10.0f);
+		NoiseOrigin1.z = Random.Range (0.0f,10.0f);
+		NoiseOrigin2.x = Random.Range (0.0f,10.0f);
+		NoiseOrigin2.z = Random.Range (0.0f,10.0f);
+		NoiseOrigin3.x = Random.Range (0.0f,10.0f);
+		NoiseOrigin3.z = Random.Range (0.0f,10.0f);
+		NoiseOrigin4.x = Random.Range (0.0f,10.0f);
+		NoiseOrigin4.z = Random.Range (0.0f,10.0f);
+
+		NoiseScale1.x = Random.Range (vScale.x, vScale.y);
+		NoiseScale1.z = NoiseScale1.x;
+
+		NoiseScale2.x = NoiseScale1.x*gScale.x;
+		NoiseScale2.z = NoiseScale1.z*gScale.x;
+
+		NoiseScale3.x = NoiseScale2.x*gScale.y;
+		NoiseScale3.z = NoiseScale2.z*gScale.y;
+
+		NoiseScale4.x = NoiseScale1.x*gScale.z;
+		NoiseScale4.z = NoiseScale1.z*gScale.z;
+	}
+
+	public void UpdateValues(){
+		NoiseScale1.z = NoiseScale1.x;
+
+		NoiseScale2.x = NoiseScale1.x*gScale.x;
+		NoiseScale2.z = NoiseScale1.z*gScale.x;
+
+		NoiseScale3.x = NoiseScale2.x*gScale.y;
+		NoiseScale3.z = NoiseScale2.z*gScale.y;
+
+		NoiseScale4.x = NoiseScale1.x*gScale.z;
+		NoiseScale4.z = NoiseScale1.z*gScale.z;
+	}
+
+	public void GenerateMap(){
+		Color[] ColorMap = new Color[MapTexture.width*MapTexture.height];
+
+		for(int x = 0; x < MapTexture.width; x++){
+			for(int z = 0; z < MapTexture.height; z++){
+				int v = GetValue(new Vector3((1.0f/Zoom)*(ViewCenter.x+x),0,(1.0f/Zoom)*(ViewCenter.y+z)));
+				ColorMap[z*MapTexture.width+x] = MapColors[v];
+			}
+		}
+
+		MapTexture.SetPixels(ColorMap);
+		MapTexture.Apply();
+	}
+
+	public float GetHumidity(Vector3 position, float altitude, float temperature){
+		//float r = RainT * (Mathf.PerlinNoise(Rain * position.x, Rain * position.z));
+		float Humidity = -0.5f * Mathf.Sin( (60.0f/Mathf.Pow(MapSize,2)) * Mathf.Pow(position.z-(MapSize/2),2) ) + WorldHumidity - (AltF*altitude);
+		if(Humidity > 1.0f){
+			Humidity = 1.0f;
+		}
+		if(Humidity < 0.0f){
+			Humidity = 0.0f;
+		}
+		return Humidity;
+	}
+		
+
+	public float GetTemperature(Vector3 position, float altitude){
+		//float r = TempT * (Mathf.PerlinNoise(Temp * position.x, Temp * position.z));
+		float Temperature = -(1.0f/(MapSize/2))*Mathf.Abs(position.z -(MapSize/2)) + WorldTemperature - (AltF*altitude);
+		if(Temperature > 1.0f){
+			Temperature = 1.0f;
+		}
+		if(Temperature < 0.0f){
+			Temperature = 0.0f;
+		}
+		return Temperature;
+	}
+
+	public float GetAltitude(Vector3 position){
+		float Base,Roughness,Detail,Negative;
+
+		float Adjustment = -(Mathf.Pow(position.x -(MapSize/2),2)/Mathf.Pow(MapSize/2,2) + (Mathf.Pow(position.z-(MapSize/2),2)/Mathf.Pow(MapSize/2,2))) + WorldHeight;
+		Base = BaseT * Mathf.PerlinNoise(MasterScale*NoiseScale1.x*(NoiseOrigin1.x+position.x),MasterScale*NoiseScale1.z*(NoiseOrigin1.z+position.z));
+		Roughness = RoughT * Mathf.PerlinNoise(MasterScale*NoiseScale2.x*(NoiseOrigin2.x+position.x),MasterScale*NoiseScale2.z*(NoiseOrigin2.z+position.z));
+		Detail = DetailT * Mathf.PerlinNoise(MasterScale*NoiseScale3.x*(NoiseOrigin3.x+position.x),MasterScale*NoiseScale3.z*(NoiseOrigin3.z+position.z));
+		Negative = NegaT * Mathf.PerlinNoise(MasterScale*NoiseScale4.x*(NoiseOrigin4.x+position.x),MasterScale*NoiseScale4.z*(NoiseOrigin4.z+position.z));
+
+		float Sample = Adjustment * ((Base+Roughness+Detail-Negative) * 0.5f);
+
+		if(Sample < 0.0f){
+			Sample = 0.0f;
+		}
+		if(Sample > 1.0f){
+			Sample = 1.0f;
+		}
+
+		return Sample;
+	}
+
+	public int GetValue(Vector3 position){
+		int Value = 13;
+
+		float Altitude = GetAltitude(position);
+
+		float Temperature = GetTemperature(position, Altitude);
+		float Humidity = GetHumidity(position,Altitude,Temperature);
+
+		if(Altitude >= SeaLevel){
+			if(Temperature < 0.25f){					//Artic
+				if(Altitude - SeaLevel < 0.025f){				//Coastal
+					Value = 3;									//Ice
+				}
+				else if(Altitude < 0.6f){						//Plains
+					if(Humidity < 0.33f){
+						Value = 4;								//Artic Desert
+					}
+					else{
+						Value = 5;								//Tundra
+					}
+				}
+				else if(Altitude >= 0.6f && Altitude < 0.8f){	//Hills
+					Value = 6;									//Tundra Hills 
+				}
+				else{										//Mountains
+					Value = 7;									//Tundra Mountains
+				}
+			}
+			else if(Temperature >= 0.25f && Temperature < 0.50f){	//Sub Artic
+				if(Altitude - SeaLevel < 0.025f){							//Coastal
+					Value = 8;												//Beach
+				}
+				else if(Altitude < 0.6f){									//Plains
+					if(Humidity < 0.33f){
+						Value = 9;											//Sub Artic Desert
+					}
+					else{
+						Value = 10;											//Taiga
+					}
+				}
+				else if(Altitude >= 0.6f && Altitude < 0.8f){				//Hills
+					Value = 11;												//Sub Artic Hills
+				}
+				else{													//Mountains
+					Value = 12;												//Sub Artic Mountains
+				}
+			}
+			else if(Temperature >= 0.5f && Temperature < 0.80f){		//Temperate
+				if(Altitude - SeaLevel < 0.025f){							//Coastal
+					Value = 13;												//Beach
+				}
+				else if (Altitude - SeaLevel < 0.05f && Humidity > 0.66f){	
+					Value = 14;												//Swamp
+				}
+				else if(Altitude < 0.6f){									//Plains
+					if(Humidity < 0.33f){
+						Value = 15;											//Desert
+					}
+					else if(Humidity >= 0.33f && Humidity < 0.66f){
+						Value = 16;											//Grasslands
+					}
+					else{
+						Value = 17;											//Forest
+					}
+				}
+				else if(Altitude >= 0.6f && Altitude < 0.8f){				//Hills
+					if(Humidity < 0.33f){
+						Value = 18;											//Desert
+					}
+					else if(Humidity >= 0.33f && Humidity < 0.66f){
+						Value = 19;											//Grasslands
+					}
+					else{
+						Value = 20;											//Forest
+					}
+				}
+				else{													//Mountains
+					Value = 21;												//Temperate Mountains
+				}
+			}
+			else if(Temperature >= 0.8f){								//Tropical
+				if(Altitude - SeaLevel < 0.025f){							//Coastal
+					Value = 22;												//Beach
+				}
+				else if (Altitude - SeaLevel < 0.05f && Humidity > 0.66f){	
+					Value = 23;												//Swamp
+				}
+				else if(Altitude < 0.6f){									//Plains
+					if(Humidity < 0.20f){
+						Value = 24;											//Desert
+					}
+					else if(Humidity >= 0.20f && Humidity < 0.40f){
+						Value = 25;											//Shrubland
+					}
+					else if(Humidity >= 0.40f && Humidity < 0.60f){
+						Value = 26;											//Savannah
+					}
+					else if(Humidity >= 0.60f && Humidity < 0.8f){
+						Value = 27;											//Forest
+					}
+					else{
+						Value = 28;											//Rainforest
+					}
+				}
+				else if(Altitude >= 0.6f && Altitude < 0.8f){				//Hills
+					if(Humidity < 0.20f){
+						Value = 29;											//Desert
+					}
+					else if(Humidity >= 0.20f && Humidity < 0.40f){
+						Value = 30;											//Shrubland
+					}
+					else if(Humidity >= 0.40f && Humidity < 0.60f){
+						Value = 31;											//Savannah
+					}
+					else if(Humidity >= 0.60f && Humidity < 0.8f){
+						Value = 32;											//Forest
+					}
+					else{
+						Value = 33;											//Rainforest
+					}
+				}
+				else{													//Mountains
+					Value = 34;												//Tropical Mountains
+				}
+			}
+
+			//In case of error
+			else{
+				Value = 0;		//Error
+			}
+		}
+		else{
+			if(Altitude < (SeaLevel*0.5f)){
+				Value = 1;		//Deep Ocean
+			}
+			else{
+
+			}
+			Value = 2;		//Shallow Ocean
+		}
+
+		return Value;
+	}//End GetValue()
+
+	public void AddCities(int number){
+		int i = 0;
+		int dx = MapSize / 4;
+		int dy = MapSize / 4;
+
+		while(i < number){
+			int x = Random.Range (dx, 3 * dx);
+			int y = Random.Range (dy, 3 * dy);
+
+			float a = GetAltitude (new Vector3 (x, 0, y));
+
+			if (a >= SeaLevel) {
+				MapTexture.SetPixel (x,y,Color.red);
+				i++;
+			}
+		}
+
+		MapTexture.Apply ();
+	}
+}
---
+++ MapGenerator.cs
@@ -0,0 +1,222 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	public int MapSize;
+	public int ChunkSize;
+	public Color[] MapColors;
+	public Texture2D MainMap;
+
+	public float scale;
+
+	public float NoiseScale1;
+
+	// Use this for initialization
+	void Start () {
+		InitializeMap ();
+		GenerateMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.G)) {
+			float StartTime = Time.realtimeSinceStartup;
+			GenerateMap ();
+			//GenerateNoiseMap();
+			float EndTime = Time.realtimeSinceStartup - StartTime;
+
+			Debug.Log ("It took " + EndTime + " seconds to generate.");
+		}
+		GenerateMap ();
+
+		//GenerateNoiseMap ();
+	}
+
+	public void InitializeMap(){
+		int h = ChunkSize * 16;
+		MainMap = new Texture2D (h, h);
+
+		MainMap.filterMode = FilterMode.Point;
+		MainMap.wrapMode = TextureWrapMode.Clamp;
+
+		gameObject.GetComponent<Renderer> ().material.mainTexture = MainMap;
+	}
+
+	public void GenerateNoiseMap(){
+		int h = ChunkSize * 16;
+		Color[] ColorMap = new Color[h * h];
+
+		float kx = (float)(ChunkSize * 16.0f);
+		float ky = (float)(ChunkSize * 16.0f);
+
+		for (int x = 0; x < h; x++) {
+			for (int y = 0; y < h; y++) {
+				float dx = (float)(x/kx);
+				float dy = (float)(y/ky);
+
+				float s = Mathf.PerlinNoise (NoiseScale1*dx, NoiseScale1*dy);
+				float Adjustment = -(Mathf.Pow((x) - (h/2),2)/Mathf.Pow(h/2,2) + (Mathf.Pow((y) - (h/2),2)/Mathf.Pow(h/2,2))) + 1.0f;
+				int b = GetValue (Adjustment*s);
+				ColorMap [y * h + x] = MapColors[b];
+			}
+		}
+
+		MainMap.SetPixels (ColorMap);
+		MainMap.Apply ();
+	}
+
+	public void GenerateMap(){
+		int h = ChunkSize * 16;
+		Color[] ColorMap = new Color[h * h];
+
+		for(int mx = 0; mx < ChunkSize; mx++){
+			for(int my = 0; my < ChunkSize; my++){
+				//Get Samples
+
+				float sx, sy;
+
+				//float s1 = Mathf.PerlinNoise(NoiseScale1*(sx),NoiseScale1*(sy));
+				//float s2 = Mathf.PerlinNoise(NoiseScale1*(sx + 1), NoiseScale1*(sy));
+				//float s3 = Mathf.PerlinNoise(NoiseScale1*(sx), NoiseScale1*(sy + 1));
+				//float s4 = Mathf.PerlinNoise(NoiseScale1*(sx + 1), NoiseScale1*(sy + 1));
+
+				sx = ((float)(mx)/(float)ChunkSize);
+				sy = ((float)(my)/(float)ChunkSize);
+				float s1 = Mathf.PerlinNoise(NoiseScale1*sx,NoiseScale1*sy);
+
+				sx = ((float)(mx+1)/(float)ChunkSize);
+				sy = ((float)(my)/(float)ChunkSize);
+				float s2 = Mathf.PerlinNoise(NoiseScale1*sx,NoiseScale1*sy);
+
+				sx = ((float)(mx)/(float)ChunkSize);
+				sy = ((float)(my+1)/(float)ChunkSize);
+				float s3 = Mathf.PerlinNoise(NoiseScale1*sx,NoiseScale1*sy);
+
+				sx = ((float)(mx+1)/(float)ChunkSize);
+				sy = ((float)(my+1)/(float)ChunkSize);
+				float s4 = Mathf.PerlinNoise(NoiseScale1*sx,NoiseScale1*sy);
+
+
+				//float d1 = 0.05f;
+				//float d2 = 0.15f;
+				//float d3 = 0.25f;
+				//float d4 = 0.35f;
+
+				//Generate Chunk
+				for (int x = 0; x < 16; x++) {
+					for (int y = 0; y < 16; y++) {
+						float cs = BiLerpSample(s1,s2,s3,s4,x,y);
+
+						//float cs = BiLerpSample (d1, d2, d3, d4, x, y);
+
+						float Adjustment = -(Mathf.Pow(((mx * 16) + x) - (h/2),2)/Mathf.Pow(h/2,2) + (Mathf.Pow(((my * 16) + y) - (h/2),2)/Mathf.Pow(h/2,2))) + 1.0f;
+
+						//float sample = Adjustment * cs;
+						//ColorMap [((my * 16) + y) * (h) + ((mx * 16) + x)] = new Color (0.0f, sample, sample);
+						int b = GetValue(Adjustment*cs);
+
+						ColorMap[((my * 16) + y) * (h) + ((mx * 16) + x)] = MapColors[b];
+					}
+				}
+			}
+		}
+
+		MainMap.SetPixels (ColorMap);
+		MainMap.Apply ();
+
+	}
+
+	public int GetValue(float a){
+		int b = 0;
+
+		if (a < 0.10f) {
+			b = 1;
+		} 
+		else if (a < 0.20f) {
+			b = 2;
+		} 
+		else if (a < 0.30f) {
+			b = 3;
+		} 
+		else if (a < 0.40f) {
+			b = 4;
+		} 
+		else if (a < 0.50f) {
+			b = 5;
+		} 
+		else if (a < 0.60f) {
+			b = 6;
+		} 
+		else if (a < 0.70f) {
+			b = 7;
+		} 
+		else if (a < 0.80f) {
+			b = 8;
+		} 
+		else if (a < 0.90f) {
+			b = 9;
+		} 
+		else {
+			b = 10;
+		}
+		return b;
+	}
+
+	public void GenerateMap1(){
+		float s1 = 0.5f;
+		float s2 = 1.0f;
+		float s3 = 0.50f;
+		float s4 = 0.25f;
+		float dsize = (float)MapSize;
+		Color[] ColorMap = new Color[MapSize * MapSize];
+
+		for (int x = 0; x < MapSize; x++) {
+			for (int y = 0; y < MapSize; y++) {
+
+				float r = BiLerpSample (MapColors[0].r,MapColors[1].r,MapColors[2].r,MapColors[3].r,x,y);
+				float g = BiLerpSample (MapColors[0].g,MapColors[1].g,MapColors[2].g,MapColors[3].g,x,y);
+				float b = BiLerpSample (MapColors[0].b,MapColors[1].b,MapColors[2].b,MapColors[3].b,x,y);
+
+				ColorMap [y * MapSize + x] = new Color (r,g,b);
+			}
+		}
+
+		MainMap.SetPixels (ColorMap);
+		MainMap.Apply ();
+	}
+
+	public float BiLinearSample(float a, float b, float c, float d, int u, int v){
+		float x1 = 0.0f;
+		float x2 = (float)16;
+		float y1 = 0.0f;
+		float y2 = (float)16;
+
+		float f1 = (((x2 - u)/(x2 - x1))*a) + (((u - x1)/(x2 - x1))*b);
+		float f2 = (((x2 - u)/(x2 - x1))*c) + (((u - x1)/(x2 - x1))*d);
+		float f3 = (((y2 - v)/(y2 - y1))*f1) + (((v - y1)/(y2 - y1))*f2);
+
+		return f3;
+	}
+
+	public float BiLerpSample (float a, float b, float c, float d, int u, int v){
+		//float x1 = 0.0f;
+		//float x2 = (float)MapSize;
+		//float y1 = 0.0f;
+		//float y2 = (float)MapSize;
+
+		float dx = 16.0f;
+		float dy = 16.0f;
+
+		float f1 = (((dx - u)/(dx))*a) + (((u)/(dx))*b);
+		float f2 = (((dx - u)/(dx))*c) + (((u)/(dx))*d);
+		float f3 = (((dy - v)/(dy))*f1) + (((v)/(dy))*f2);
+
+		return f3;
+	}
+
+	public float SampleLerp(float a, float b, float t){
+		return (1.0f - t) * a + (t * b);
+	}
+}
